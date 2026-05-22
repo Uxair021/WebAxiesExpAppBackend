70ff8175858904c4472ab9a623096e0c56e31b62
Code-Mental <workinghours123456@gmail.com>
2026-05-22 12:26:59 +0500

    controller update for edit update code

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,284 +1,156 @@
-const Expense =
-require("../models/Expense");
+const Expense = require("../models/Expense");
 
+/* =========================
+      CREATE
+========================= */
 
+exports.createExpense = async (req, res) => {
+  try {
+    const expenseData = {
+      date: req.body.date || "",
 
-/* ---------------- CREATE ---------------- */
+      client: req.body.client || "",
 
-exports.createExpense =
-async (req,res)=>{
+      project: req.body.project || "",
 
-try{
+      desc: req.body.desc || "",
 
-const expenseData = {
+      received:
+        req.body.received === "" ||
+        req.body.received === "null" ||
+        req.body.received == null
+          ? 0
+          : Number(req.body.received),
 
-date:
-req.body.date,
+      expense:
+        req.body.expense === "" ||
+        req.body.expense === "null" ||
+        req.body.expense == null
+          ? 0
+          : Number(req.body.expense),
+    };
 
-client:
-req.body.client,
+    if (req.files && req.files.length > 0) {
+      expenseData.image = req.files[0].filename;
+    }
 
-project:
-req.body.project,
+    const expense = await Expense.create(expenseData);
 
-desc:
-req.body.desc,
+    res.status(201).json(expense);
+  } catch (error) {
+    console.log(error);
 
-received:
-req.body.received,
-
-expense:
-req.body.expense
-
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
+/* =========================
+        GET ALL
+========================= */
 
-if(
-req.files &&
-req.files.length > 0
-){
+exports.getExpenses = async (req, res) => {
+  try {
+    const expenses = await Expense.find().sort({ _id: -1 });
 
-expenseData.image =
-req.files[0].filename;
-
-}
-
-
-const expense =
-await Expense.create(
-expenseData
-);
-
-
-res
-.status(201)
-.json(expense);
-
-}
-
-catch(error){
-
-console.log(error);
-
-res
-.status(500)
-.json({
-
-message:
-error.message
-
-});
-
-}
-
+    res.json(expenses);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
+/* =========================
+        UPDATE
+========================= */
 
+exports.editExpense = async (req, res) => {
+  try {
+    const expenseId = req.params.id;
 
+    const updatedData = {
+      date: req.body.date || "",
 
-/* ---------------- GET ALL ---------------- */
+      client: req.body.client || "",
 
-exports.getExpenses =
-async(req,res)=>{
+      project: req.body.project || "",
 
-try{
+      desc: req.body.desc || "",
 
-const expenses =
-await Expense.find()
-.sort({_id:-1});
+      received:
+        req.body.received === "" ||
+        req.body.received === "null" ||
+        req.body.received == null
+          ? 0
+          : Number(req.body.received),
 
-res.json(
-expenses
-);
+      expense:
+        req.body.expense === "" ||
+        req.body.expense === "null" ||
+        req.body.expense == null
+          ? 0
+          : Number(req.body.expense),
+    };
 
-}
+    if (req.files && req.files.length > 0) {
+      updatedData.image = req.files[0].filename;
+    }
 
-catch(error){
+    const updatedExpense = await Expense.findByIdAndUpdate(
+      expenseId,
 
-res
-.status(500)
-.json({
+      updatedData,
 
-message:
-error.message
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
 
-});
+    if (!updatedExpense) {
+      return res.status(404).json({
+        message: "Expense not found",
+      });
+    }
 
-}
+    res.status(200).json(updatedExpense);
+  } catch (error) {
+    console.log(error);
 
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
-
-
-
-/* ---------------- UPDATE ---------------- */
-
-exports.editExpense =
-async(req,res)=>{
-
-try{
-
-const expenseId =
-req.params.id;
-
-
-const updatedData = {
-
-date:
-req.body.date,
-
-client:
-req.body.client,
-
-project:
-req.body.project,
-
-desc:
-req.body.desc,
-
-received:
-req.body.received,
-
-expense:
-req.body.expense
-
-};
-
-
-
-if(
-req.files &&
-req.files.length > 0
-){
-
-updatedData.image =
-req.files[0].filename;
-
-}
-
-
-
-const updatedExpense =
-await Expense.findByIdAndUpdate(
-
-expenseId,
-
-updatedData,
-
-{
-new:true
-}
-
-);
-
-
-
-if(
-!updatedExpense
-){
-
-return res
-.status(404)
-.json({
-
-message:
-"Expense not found"
-
-});
-
-}
-
-
-
-res
-.status(200)
-.json(
-updatedExpense
-);
-
-}
-
-catch(error){
-
-console.log(error);
-
-res
-.status(500)
-.json({
-
-message:
-error.message
-
-});
-
-}
-
-};
-
-
-
-
-
-/* ---------------- DELETE ---------------- */
-
-exports.deleteExpense =
-async(req,res)=>{
-
-try{
-
-const expenseId =
-req.params.id;
-
-
-const deletedExpense =
-await Expense.findByIdAndDelete(
-expenseId
-);
-
-
-if(
-!deletedExpense
-){
-
-return res
-.status(404)
-.json({
-
-message:
-"Expense not found"
-
-});
-
-}
-
-
-
-res
-.status(200)
-.json({
-
-message:
-"Expense deleted successfully",
-
-deletedExpense
-
-});
-
-}
-
-catch(error){
-
-console.log(error);
-
-res
-.status(500)
-.json({
-
-message:
-error.message
-
-});
-
-}
-
+/* =========================
+        DELETE
+========================= */
+
+exports.deleteExpense = async (req, res) => {
+  try {
+    const expenseId = req.params.id;
+
+    const deletedExpense = await Expense.findByIdAndDelete(expenseId);
+
+    if (!deletedExpense) {
+      return res.status(404).json({
+        message: "Expense not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Expense deleted successfully",
+
+      deletedExpense,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
